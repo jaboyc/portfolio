e8622cdbe01d5e07fff341b6bfb8258c1b394fc9
jaboyc <jakeboychenko@yahoo.com>
2023-12-20 12:43:04 -0500

    Improve project and blog page with SEO

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,15 +1,46 @@
 import Footer from '@/app/ui/footer';
 import Header from '@/app/ui/header';
+import Renderable from '@/app/ui/renderable';
 import { PrismaClient } from '@prisma/client';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 export const revalidate = 60 * 60 * 12;
 
-export default async function Page({ params }: { params: { slug: string } }) {
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata({
+  params,
+}: Props): Promise<Metadata | null> {
   const prisma = new PrismaClient();
   const blogPost = await prisma.blogPost.findUnique({
     where: {
       slug: params.slug,
+    },
+  });
+
+  if (!blogPost) {
+    return null;
+  }
+
+  return {
+    title: blogPost.title,
+    description: blogPost.subtitle,
+    authors: [{ name: 'Jake Boychenko' }],
+    keywords: blogPost.keywords,
+  };
+}
+
+export default async function Page({ params }: Props) {
+  const prisma = new PrismaClient();
+  const blogPost = await prisma.blogPost.findUnique({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      renderable: true,
     },
   });
 
@@ -21,10 +52,19 @@ export default async function Page({ params }: { params: { slug: string } }) {
     <>
       <Header />
       <section>
-        <h2 className="text-start text-white">{blogPost.title}</h2>
-        <p className="text-start text-primary">{blogPost.subtitle}</p>
+        <div className="flex flex-wrap gap-4 items-center justify-center">
+          <Renderable
+            renderable={blogPost.renderable}
+            width={350}
+            color={'white'}
+          />
+          <div className="flex flex-col flex-grow basis-[650px]">
+            <h2 className="text-white">{blogPost.title}</h2>
+            <p className="text-primary">{blogPost.subtitle}</p>
+          </div>
+        </div>
       </section>
-      <section className="bg-white">
+      <section className="bg-white items-center">
         <p className="subbody">{blogPost.body}</p>
       </section>
       <Footer />
--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -1,15 +1,46 @@
 import Footer from '@/app/ui/footer';
 import Header from '@/app/ui/header';
+import Renderable from '@/app/ui/renderable';
 import { PrismaClient } from '@prisma/client';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 export const revalidate = 60 * 60 * 12;
+
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata({
+  params,
+}: Props): Promise<Metadata | null> {
+  const prisma = new PrismaClient();
+  const project = await prisma.project.findUnique({
+    where: {
+      slug: params.slug,
+    },
+  });
+
+  if (!project) {
+    return null;
+  }
+
+  return {
+    title: project.name,
+    description: project.shortDescription,
+    authors: [{ name: 'Jake Boychenko' }],
+    keywords: project.keywords,
+  };
+}
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const prisma = new PrismaClient();
   const project = await prisma.project.findUnique({
     where: {
       slug: params.slug,
+    },
+    include: {
+      renderable: true,
     },
   });
 
@@ -21,10 +52,20 @@ export default async function Page({ params }: { params: { slug: string } }) {
     <>
       <Header />
       <section>
-        <h2 className="text-start text-white">{project.name}</h2>
+        <div className="flex flex-wrap gap-4 items-center justify-center">
+          <Renderable
+            renderable={project.renderable}
+            width={350}
+            color={'white'}
+          />
+          <div className="flex flex-col flex-grow basis-[650px]">
+            <h2 className="text-start text-white">{project.name}</h2>
+            <p className="text-primary">{project.shortDescription}</p>
+          </div>
+        </div>
       </section>
       <section className="bg-white">
-        <p className="subbody">{project.shortDescription}</p>
+        <p className="subbody">{project.body}</p>
       </section>
       <Footer />
     </>
