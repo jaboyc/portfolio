91c640444e6c71f98192633804284a0d63f4c3cf
jaboyc <jakeboychenko@yahoo.com>
2023-12-26 14:52:45 -0500

    Add GA_ID as public.

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -43,14 +43,14 @@ export default function RootLayout({
           <>
             <Script
               async
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
             />
             <Script id="google-analytics">
               {`window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
-                gtag('config', '${process.env.GA_ID}');`}
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`}
             </Script>
           </>
         )}
